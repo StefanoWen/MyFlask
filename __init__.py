f6d6e31c247dc0f941bbafa67e5a8b8d9054aa6d
Wenmingji <Theyn@foxmail.com>
2018-03-14 16:31:10 +0800

    project commit

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/admin')
+def hello_world():
+    return 'Hello World!'
+
+
+# @app.route('/admin')
+# def admin():
+#     return 'admin page'
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
