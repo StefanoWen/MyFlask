abebf40d86a8f0f83fe6d1c12f015a860babc430
StefaNoWen <Theyn@foxmail.com>
2018-04-14 01:02:21 +0800

    添加模板文件

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/index')
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 @app.route('/admin')
 def admin():
     return 'admin page'
+
+
+@app.route('/UA')
+def UA():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is % s</p>' % user_agent
 
 
 if __name__ == '__main__':
